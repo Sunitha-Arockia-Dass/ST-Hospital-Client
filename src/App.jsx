16b8602a47da943d2d59e04401a63247f4a471a5
Thomas-LBS <thomas.lebas26@gmail.com>
2023-11-07 12:32:22 +0100

    Login and Signup pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom"
+/* Components */
+import Navbar from "./components/Navbar"
+
+/* Pages */
+import HomePage from "./pages/HomePage"
+
+import SignUp from "./pages/auth/SignUp"
+import LogIn from "./pages/auth/LogIn"
+
+import NotFound from "./pages/error/NotFound"
+import Error from "./pages/error/Error"
+
 
 function App() {
-  
 
   return (
     <>
-      <h1>React and CSS initialized</h1>
+      <Navbar />
+
+      <Routes>
+        {/*   Add <Route /> components between <Routes> and </Routes>   */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* Auth Pages */}
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+
+        {/* Error Pages */}
+        <Route path="/500" element={<Error />} />
+        <Route path="*" element={<NotFound />} />        
+      </Routes>
     </>
   )
 }
---
+++ src/pages/error/NotFound.jsx
@@ -0,0 +1,11 @@
+function NotFound (){
+    return(
+        <div className="notfound">
+            <h1>Oops ! Something&apos;s missing... 404</h1>
+            <p>Check if everything is ok! Try again or come back <a href="/">Home</a>.
+        </p>
+        </div>
+    )
+}
+
+export default NotFound
