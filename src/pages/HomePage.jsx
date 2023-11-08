cf111c61e996fdaabaad282aa447fe540bae8738
Thomas-LBS <thomas.lebas26@gmail.com>
2023-11-08 12:15:43 +0100

    DarkMode Animation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,10 +2,6 @@ function HomePage() {
     return (
         <div className="homepage">
             <h1>Home is the best place on earth</h1>
-
-            <button>
-                Hey look, this is only CSS!                
-            </button>
         </div>
     )
 }
