39449314018964d750734d01f590169e2818058a
Sunitha-Arockia-Dass <135265573+Sunitha-Arockia-Dass@users.noreply.github.com>
2023-11-10 09:40:07 +0100

    login functions basic

--- src/pages/auth/LogIn.jsx
+++ src/pages/auth/LogIn.jsx
@@ -1,6 +1,29 @@
 import { NavLink } from "react-router-dom"
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 
 function LogIn() {
+    // const [user, setUser] = useState();
+    const login = (e) => {
+        e.preventDefault();
+        let data = {
+          name: e.target.username.value,
+          password: e.target.password.value,
+        };
+        console.log(data);
+        axios
+          .post("http://localhost:5005/auth/login", data)
+          .then((response) => {
+            console.log("logged in up successfully");
+            console.log(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      };
+
+
     return (
         <div id="login">
             <h2>Log In</h2>
@@ -8,7 +31,7 @@ function LogIn() {
                 <NavLink to="/signup">signup</NavLink>
             </p>
 
-            <form action="/login" method="POST">
+            <form onSubmit={login}>
                 <label>Username</label>
                 <input type="username" name="username" placeholder="" />
                 
--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -1,29 +1,49 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function SignUp() {
-    return (
-        <div id="signup">
-            <h2>Sign Up</h2>
-            <p>Already registered ?
-                <NavLink to="/login">login</NavLink>
-            </p>
+//   const [user, setUser] = useState();
 
-            <form action="/signup" method="POST">
+  const signup = (e) => {
+    e.preventDefault();
+    let data = {
+      name: e.target.username.value,
+      email: e.target.email.value,
+      password: e.target.password.value,
+    };
+    console.log(data);
+    axios
+      .post("http://localhost:5005/auth/signup", data)
+      .then((response) => {
+        console.log("Signed up successfully");
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div id="signup">
+      <h2>Sign Up</h2>
+      <p>
+        Already registered ?<NavLink to="/login">login</NavLink>
+      </p>
 
-                <label>Username</label>
-                <input type="text" name="username" placeholder="" />
-                
-                <label>Email</label>
-                <input type="email" name="email" placeholder="" />
-                
-                <label>Password</label>
-                <input type="password" name="password" placeholder="" />
-                
-                <button type="submit">Sign Up</button>
-            </form>
-            
-        </div>
-    )
+      <form onSubmit={signup}>
+        <label>Username</label>
+        <input type="text" name="username" placeholder="" />
+
+        <label>Email</label>
+        <input type="email" name="email" placeholder="" />
+
+        <label>Password</label>
+        <input type="password" name="password" placeholder="" />
+
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
