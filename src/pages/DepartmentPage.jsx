adef336f2a7c172eb5c2f6446169f3fe6ff3c7bb
Thomas-LBS <thomas.lebas26@gmail.com>
2023-11-25 15:48:23 +0100

    better merge

--- src/pages/DepartmentPage.jsx
+++ src/pages/DepartmentPage.jsx
@@ -7,7 +7,6 @@ function DepartmentPage() {
   const [departments, setDepartments] = useState();
   const [selectedDept, setSelectedDept] = useState(null);
   const [doctor, setDoctor] = useState(null);
-
   useEffect(() => {
     axios.get(URL.departments).then((foundDepartments) => {
       setDepartments(foundDepartments.data);
@@ -23,71 +22,36 @@ function DepartmentPage() {
     <div className="dpt-page full">
       {!doctor && (
         <div className="container breakout">
-        <h3>Department Page</h3>
+          <h3>Department Page</h3>
         </div>
       )}
-      {doctor ? (
-        <SingleDoctor doctor={doctor} selectedDept={selectedDept} setDoctor={setDoctor} />
-      ) : (
-        <div className="all-dpt">
-          {selectedDept ? (
-            <SingleDept
-              selectedDept={selectedDept}
-              setSelectedDept={setSelectedDept}
-              setDoctor={setDoctor}
-            />
-          ) : (
-            departments?.map((department) => {
-              return (
-                <div className="one-dpt"
-                  key={department._id}
-                  to={`/department/${department._id}`}
-                  state={{ department }}
-                >
-                  <fieldset>
-                    <legend><h4>{department.name}</h4></legend>
-                    <p>{department.description}</p>
-                    <div className="dpt-img">
-                      <img src={department.image} alt="Department icon" />
-                    </div>
-                  </fieldset>
-                </div>
-              );
-            })
-          )}
-        </div>
-      )} */}
+      <div className="all-dpt">
+        {departments?.map((department) => {
+          return (
 
-      {departments?.map((department) => {
-              return (
-                  <Link
-                    key={department._id}
-                    to={`/departments/${department._id}`}
-                    state={{ department }}
-                  >
-                <div className="one-dpt" onClick={() => displayDept(department._id)}>
-                    <fieldset>
-                      <img src={department.image} className="dpt-img" alt="Department icon" />
-                      <legend>
-                        <h4>{department.name}</h4>
-                      </legend>
-                      <p>{department.description}</p>
-                      <div className="dpt-img">
-                        <img src={department.image} alt="Department icon" />
-                      </div>
-                    </fieldset>
-                </div>
-                  </Link>
-              );
-            })
-          )}
-        </div>
-      )}
-
-
+            <Link
+              key={department._id}
+              to={`/departments/${department._id}`}
+              state={{ department }}
+            >
+              <div className="one-dpt" onClick={() => displayDept(department._id)}>
+                <fieldset>
+                  <img src={department.image} className="dpt-img" alt="Department icon" />
+                  <legend>
+                    <h4>{department.name}</h4>
+                  </legend>
+                  <p>{department.description}</p>
+                  <div className="dpt-img">
+                    <img src={department.image} alt="Department icon" />
+                  </div>
+                </fieldset>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
     </div>
-  )
-
+  );
 }
 
 export default DepartmentPage;
