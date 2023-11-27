f9e8a4a0813efe4b9ec828cfab4b68128c73c57b
Thomas-LBS <thomas.lebas26@gmail.com>
2023-11-27 14:18:24 +0100

    agenda design

--- src/components/DoctorCalendarComponent.jsx
+++ src/components/DoctorCalendarComponent.jsx
@@ -265,8 +265,7 @@ const DoctorCalendarComponent = ({
         : selectedSlot && (
           <div>
             <button className="book-appointment" onClick={creatAppt} disabled={!selectedSlot}>
-            <svg width="100px" height="100px" viewBox="0 0.75 24.00 24.00" fill="none"><path d="M15 12L12 12M12 12L9 12M12 12L12 9M12 12L12 15" stroke-width="1.5" stroke-linecap="round"></path> <path d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8" stroke-width="1.5" stroke-linecap="round"></path></svg>
-            </button>
+            <svg width="100px" height="100px" viewBox="0 0 24 24" fill="none"><path d="M2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12Z" stroke-width="1.5"></path> <path d="M15 12L12 12M12 12L9 12M12 12L12 9M12 12L12 15" stroke-width="1.5" stroke-linecap="round"></path></svg></button>
             <button className="back" onClick={handleBackToMonth}>Back to Month</button>
           </div>
           )}
