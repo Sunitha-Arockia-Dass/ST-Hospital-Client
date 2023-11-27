e8bb12723a98a6a62a116cd9856d8b237447beba
Sunitha-Arockia-Dass <135265573+Sunitha-Arockia-Dass@users.noreply.github.com>
2023-11-27 15:51:54 +0100

    signup page edited

--- src/components/PatientPersonalInfo.jsx
+++ src/components/PatientPersonalInfo.jsx
@@ -8,17 +8,20 @@ import URL from "../links/links.json";
 
 function PatientPersonalInfo(){
     const { user } = useContext(AuthContext);
-    const [gp, setGP] = useState();
+    const [gp, setGP] = useState(null);
 
     useEffect(() => {
+      if(user.patientDetails){
+
         axios
-          .get(`${URL.gPractice}/${user.patientDetails.gp[0]._id}`)
-          .then((response) => {
-            // console.log(response.data);
-            setGP(response.data);
-          })
-          .catch((error) => console.log("error", error));
-      }, []);
+        .get(`${URL.gPractice}/${user.patientDetails.gp[0]._id}`)
+        .then((response) => {
+          // console.log(response.data);
+          setGP(response.data);
+        })
+        .catch((error) => console.log("error", error));
+      }
+      }, [user.patientDetails]);
     
 
 return(
@@ -30,12 +33,12 @@ return(
       <h5>Last Name:{user.lastname}</h5>
       <h6>Username:{user.username}</h6>
       <h6>Email:{user.email}</h6>
-      <h6>DOB:{user.patientDetails.dateOfBirth}</h6>
+      {user.patientDetails && <div><h6>DOB:{user.patientDetails.dateOfBirth}</h6>
       <h6>Phone:{user.patientDetails.contactNumber}</h6>
       <h6>General Practice:{gp?.name}</h6>
       <p>Address:{user.patientDetails.address.street} {user.patientDetails.address.houseNumber} </p>
       <p>{user.patientDetails.address.postalCode} {user.patientDetails.address.city} </p>
-      <p>{user.patientDetails.address.country}</p>
+      <p>{user.patientDetails.address.country}</p></div>}
       <NavLink to="/editaccount">
         
         <button>Edit Account</button>
