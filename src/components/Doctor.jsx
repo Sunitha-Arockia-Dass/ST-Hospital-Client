a84f1b61b46dcc0777391b7646efd4c2da6d3e8e
Thomas-LBS <thomas.lebas26@gmail.com>
2023-12-06 15:59:23 +0100

    doctor design

--- src/components/Doctor.jsx
+++ src/components/Doctor.jsx
@@ -5,11 +5,20 @@ function Doctor() {
   const { user } = useContext(AuthContext);
 console.log('user',user)
   return (
-    <div>
-      <h1>
-        Welcome Dr.{user.firstname} {user.lastname}
-      </h1>
+    <div id="account" className="dpt-page">
+    <div className="main-frame">
+    
+      <h3>
+        Dr {user.firstname} {user.lastname}
+        <h5>Great doctor, what do you want to check ?</h5>
+      </h3>
+      
+
+    
       <DoctorCalendarComponent doctor={user.doctor} />
+      
+    
+    </div>
     </div>
   );
 }
--- src/components/ViewPatientRecord.jsx
+++ src/components/ViewPatientRecord.jsx
@@ -50,46 +50,54 @@ function ViewPatientRecord({
   }
 // console.log('start',records[0].record.appointment.start)
   return (
-    <div>
-      <h1>PATIENT RECORD PAGE</h1>
+    <div className="appt-details">   
+
+          <h4>Patient Records</h4>
+   
+        <div className="appt-view">
+    
+      
+      
       {singleRecordView ? (
-        <>
-        <button onClick={()=>{setSingleRecordView(false)}}>Back</button>
-          <p><strong>Blood Pressure :</strong>
+        <div className="one-appointment2">
+          <p><strong>Blood Pressure: </strong>
           <span style={getVitalsStyle(singleRecord.vitals.bloodPressure.range)}>
 
           {singleRecord.vitals.bloodPressure.value} 
            ({singleRecord.vitals.bloodPressure.range}) </span></p>
-          <p><strong>Heart Rate :</strong>
+          <p><strong>Heart Rate: </strong>
           <span style={getVitalsStyle(singleRecord.vitals.bloodPressure.range)}>
 
           {singleRecord.vitals.heartRate.value}  ({singleRecord.vitals.heartRate.range}) </span></p>
-          <p><strong>Pulse rate :</strong>
+          <p><strong>Pulse rate: </strong>
           <span style={getVitalsStyle(singleRecord.vitals.bloodPressure.range)}>
 
           {singleRecord.vitals.pulseRate.value}  ({singleRecord.vitals.pulseRate.range}) </span></p>
-          <p><strong>Temperature :</strong>
+          <p><strong>Temperature: </strong>
           <span style={getVitalsStyle(singleRecord.vitals.bloodPressure.range)}>
 
           {singleRecord.vitals.temperature.value}  ({singleRecord.vitals.temperature.range}) </span></p>
-          <p><strong>Complaints :</strong>{singleRecord.complaints}  </p>
-          <p><strong>Description :</strong>{singleRecord.description}  </p>
-          <p><strong>Prescribed Medications :</strong>{singleRecord.prescribedMedications}</p>
-                  <p><strong>Record Created By : Dr {singleRecord.doctor.firstname} {singleRecord.doctor.lastname} </strong></p>       
-
-        </>
+          <p><strong>Complaints: </strong>{singleRecord.complaints}  </p>
+          <p><strong>Description: </strong>{singleRecord.description}  </p>
+          <p><strong>Prescribed Medications: </strong>{singleRecord.prescribedMedications}</p>
+                  <p><strong>Record Created By: Dr {singleRecord.doctor.firstname} {singleRecord.doctor.lastname} </strong></p>       
+        <button className="back" onClick={()=>{setSingleRecordView(false)}}>↩ Back</button>
+        </div>
       ) : (
         <>
           {records?.record.map((record) => {
             return (
-              <div key={record._id} onClick={()=>{showSingleRecord(record)}}>
+              <button className="back one-appointment" key={record._id} onClick={()=>{showSingleRecord(record)}}>
                 <p>{formatDateTime(record.appointment.start)} </p>
-              </div>
+              </button>
             );
           })}
         </>
       )}
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
+      </div>
+  
+      {errorMessage && <p className="error-message">{errorMessage}</p>}  
+   
     </div>
   );
 }
